a0ed368d9a9694ab3300476ece5056320db501a7
naveen <naveenbaloni018@gmail.com>
2025-01-01 21:55:02 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,10 +51,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messagesRoutes);
 
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname,'../frontend/dist')));
+  app.use(express.static(path.resolve(__dirname,'../frontend/dist')));
 
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    res.sendFile(path.resolve(__dirname,'../frontend/dist/index.html'));
   });
 }
 
