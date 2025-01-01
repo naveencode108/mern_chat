885932b3359044ae126ba28afe42330c7b9e59c3
naveen <naveenbaloni018@gmail.com>
2025-01-01 21:33:49 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,11 @@ import http from 'http';
 
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const server = http.createServer(app);
